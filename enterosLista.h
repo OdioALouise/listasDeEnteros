06abe764f6c8879cf275db2f704d31ee8c6eed14
Dahiana <dahiaa68@gmail.com>
2020-11-04 16:58:29 -0300

    primera version de MCM

--- enterosLista.h
+++ enterosLista.h
@@ -15,8 +15,8 @@ bool esEnLista(Enteros *listaEnteros, int n);
 void eliminarNodo(Enteros *listaEnteros, int n);
 Enteros* divisores(Enteros *listaEnteros, int n);
 Enteros* divisoresComunes(Enteros *listaEnteros, int n, int m);
-int maximoComunDivisor(Enteros *listaEnteros, int n, int m);
-int minimoComunMultiplo(Enteros *listaEnteros, int n, int m);
+int maximoComunDivisor(int n, int m);
+int minimoComunMultiplo(int n, int m);
 Enteros* coprimos(Enteros *listaEnteros, int n);
 
 #endif
